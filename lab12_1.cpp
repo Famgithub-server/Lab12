a6fc43130c7c89cd8891d75db9a5cfe175b10b4d
Famgithub-server <fam0637186002@gmail.com>
2025-12-30 14:33:19 +0700

    update

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
+long long nums[50] = {0};
 long long fibonacci(int n);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
 long long fibonacci(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
-    return fibonacci(n-1) + fibonacci(n-2);
+    if (!nums[n]) nums[n] = fibonacci(n-1) + fibonacci(n-2);
+    return nums[n];
 }
